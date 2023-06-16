31884a1d8e8af75dbc624ca036b4fff3df35d242
skanredfield <skan.redfield@gmail.com>
2023-06-16 22:28:58 +0300

    Added allowed hosts
    Modified the list of allowed hosts to include the Digital Ocean server;
    Added the entrypoint file for Docker.

--- astrosite/astrosite/settings.py
+++ astrosite/astrosite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '46.101.182.213']
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
